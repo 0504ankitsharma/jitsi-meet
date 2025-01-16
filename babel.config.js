a8ec7e8ed7d2f7946b5543973516edd838b7b786
Ankit Sharma <114983967+0504ankitsharma@users.noreply.github.com>
2025-01-16 15:13:33 +0530

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-    presets: [ 'module:metro-react-native-babel-preset' ],
-    env: {
-        production: {
-            plugins: [ 'react-native-paper/babel' ]
-        }
-    },
+  presets: ["@babel/preset-env"],
+  ignore: [
+    "**/node_modules/@jitsi/rnnoise-wasm/**",
+    "**/node_modules/@vladmandic/human/**",
+    "**/node_modules/@jitsi/excalidraw/**"
+  ]
+};,
 
     // This happens because react native has conflict with @babel/plugin-transform-private-methods plugin
     // https://github.com/ethers-io/ethers.js/discussions/4309#discussioncomment-6694524
